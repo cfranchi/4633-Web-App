6b950a31bb9c1f17a04447473a23f576b18f0fd8
cfranchi <cameron.franchi3@gmail.com>
2020-11-27 11:06:13 -0500

    Add files via upload

--- searchmatches/searchbyteam.js
+++ searchmatches/searchbyteam.js
@@ -10,16 +10,17 @@ $(document).ready(function() {
                 success: function(response) {
                     $("#search_results_div").html("");
                     var data = $.parseJSON(response);
-                    var tbl=$("<table/>").attr("id","search_results_table");
+                    var tbl = $("<table/>").attr("id","search_results_table");
                     $("#search_results_div").append(tbl);
                     for(var i = 0; i < data.length; i++)
                         {
-                            var tr="<tr>";
-                            var td1="<td>" + data[i]["team1"] + "</td>";
-                            var td2="<td>" + data[i]["team2"] + "</td>";
-                            var td3="<td>" + data[i]["location"] + "</td>";
-                            var td4="<td>" + data[i]["date"] + "</td></tr>";
-                            $("#search_results_table").append(tr + td1 + td2 + td3 + td4); 
+                            var tr = "<tr>";
+                            var check = "<td><input type='checkbox' id='check_" + i + "'/></td>";
+                            var td1 = "<td>" + data[i]["team1"] + "</td>";
+                            var td2 = "<td>" + data[i]["team2"] + "</td>";
+                            var td3 = "<td>" + data[i]["location"] + "</td>";
+                            var td4 = "<td>" + data[i]["date"] + "</td></tr>";
+                            $("#search_results_table").append(tr + check + td1 + td2 + td3 + td4); 
                         }
                 }
             });
