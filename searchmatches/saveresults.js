20911f367ced0a2abd9c34421a019e7d5574fd44
cfranchi <cameron.franchi3@gmail.com>
2020-11-27 14:08:12 -0500

    Add files via upload

--- searchmatches/saveresults.js
+++ searchmatches/saveresults.js
@@ -1,7 +1,8 @@
-function saveResults(save_button_arr) {
+function saveResults(response, save_button_arr) {
+    var data = $.parseJSON(response);
     for (var i = 0; i < save_button_arr.length; i++) {
         $("#add_button_" + i).click(function() {
-            console.log(i);
+            console.log(data[i]);
         });
     }
 }
