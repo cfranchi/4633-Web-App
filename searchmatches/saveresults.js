3aab317ec8fa3e2677cab999ae6ed7b843b82a85
cfranchi <cameron.franchi3@gmail.com>
2020-11-27 14:13:12 -0500

    Update saveresults.js

--- searchmatches/saveresults.js
+++ searchmatches/saveresults.js
@@ -2,7 +2,7 @@ function saveResults(response, save_button_arr) {
     var data = $.parseJSON(response);
     for (var i = 0; i < save_button_arr.length; i++) {
         $("#add_button_" + i).click(function() {
-            console.log(data[i]);
+            console.log(data);
         });
     }
 }
