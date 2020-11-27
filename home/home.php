82aeb3a690df9f06104a66b3091ac143e2bee510
cfranchi <cameron.franchi3@gmail.com>
2020-11-26 19:55:10 -0500

    Add files via upload

--- home/home.php
+++ home/home.php
@@ -6,12 +6,15 @@
     <div id="search_matches_div">
         <input type="text" class="textbox" id="team1" name="team1" placeholder="Search by team name"/>
         <button id="search_team_button">search</button>
+        <br>
         <input type="text" class="textbox" id="location" name="location" placeholder="Search by location"/>
         <button id="search_location_button">search</button>
+        <br>
         <input type="text" class="textbox" id="month" name="month" placeholder="Search by month"/>
         <button id="search_month_button">search</button>
+        <br>
     </div>
+    <h4>RESULTS</h4>
     <div id="search_results_div">
-        RESULTS
     </div>
 </div>
