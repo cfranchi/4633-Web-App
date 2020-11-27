7dc8386db0db75833b72740e40795816142396f9
cfranchi <cameron.franchi3@gmail.com>
2020-11-26 20:48:00 -0500

    Add files via upload

--- home/home.php
+++ home/home.php
@@ -6,15 +6,16 @@
     <div id="search_matches_div">
         <input type="text" class="textbox" id="team1" name="team1" placeholder="Search by team name"/>
         <button id="search_team_button">search</button>
-        <br>
+        <br><br>
         <input type="text" class="textbox" id="location" name="location" placeholder="Search by location"/>
         <button id="search_location_button">search</button>
-        <br>
+        <br><br>
         <input type="text" class="textbox" id="month" name="month" placeholder="Search by month"/>
         <button id="search_month_button">search</button>
-        <br>
+        <br><br>
     </div>
     <h4>RESULTS</h4>
     <div id="search_results_div">
+        <table id="table" align="center" border="1px"></table>
     </div>
 </div>
--- searchmatches/searchbyteam.js
+++ searchmatches/searchbyteam.js
@@ -8,7 +8,32 @@ $(document).ready(function() {
                     team1: team1
                 },
                 success: function(response) {
-                    $("#search_results_div").html(response);
+                    var list = response;
+                    var cols = [];               
+                    for (var i = 0; i < list.length; i++) { 
+                        for (var k in list[i]) { 
+                            if (cols.indexOf(k) === -1) {  
+                                cols.push(k); 
+                            } 
+                        } 
+                    }
+                    var table = document.createElement("table"); 
+                    var tr = table.insertRow(-1); 
+                    for (var i = 0; i < cols.length; i++) { 
+                        var theader = document.createElement("th"); 
+                        theader.innerHTML = cols[i]; 
+                        tr.appendChild(theader); 
+                    } 
+                    for (var i = 0; i < list.length; i++) { 
+                        trow = table.insertRow(-1); 
+                        for (var j = 0; j < cols.length; j++) { 
+                            var cell = trow.insertCell(-1); 
+                            cell.innerHTML = list[i][cols[j]]; 
+                        } 
+                    } 
+                    var el = document.getElementById("table"); 
+                    el.innerHTML = ""; 
+                    el.appendChild(table);
                 }
             });
     });
