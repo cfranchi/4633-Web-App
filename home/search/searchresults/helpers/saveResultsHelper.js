3b2eea9677ad8fe12ed7d4a7be340fc2d8db7fe9
cfranchi <cameron.franchi3@gmail.com>
2020-11-28 00:53:05 -0500

    Add files via upload

--- home/search/searchresults/helpers/saveResultsHelper.js
+++ home/search/searchresults/helpers/saveResultsHelper.js
@@ -1,13 +1,13 @@
 function saveResults(response, save_button_arr) {
     var data = $.parseJSON(response);
     for (var i = 0; i < save_button_arr.length; i++) {
-        console.log(data);
         $("#add_button_" + i).click(function() {
             i = i - 1;
             var team1 = data[i]["team1"];
             var team2 = data[i]["team2"];
             var location = data[i]["location"];
             var date = data[i]["date"];
+            console.log(username, team1, team2, location, date);
             $.ajax({
                 url: './home/search/searchresults/searchResultsController.php',
                 type: 'post',
