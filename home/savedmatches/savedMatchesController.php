987f176b99996731ce72ea9cdfc3067e286e8fab
cfranchi <cameron.franchi3@gmail.com>
2020-11-28 12:34:47 -0500

    Update savedMatchesController.php

--- home/savedmatches/savedMatchesController.php
+++ home/savedmatches/savedMatchesController.php
@@ -28,4 +28,20 @@ class Match {
         return $this->date;
     }
 }
+
+class Schedule {
+    public $matches;
+
+    public function __construct() {
+        $this->matches = [];
+    }
+
+    public function addMatch($match) {
+        $this->matches[] = $match;
+    }
+
+    public function returnMatches() {
+        return $this->matches;
+    }
+}
 ?>
