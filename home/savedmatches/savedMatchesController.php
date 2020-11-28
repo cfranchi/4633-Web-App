1a577c93fb538d5bc32efd23c5f4a2cf07eff015
cfranchi <cameron.franchi3@gmail.com>
2020-11-28 12:36:53 -0500

    Update savedMatchesController.php

--- home/savedmatches/savedMatchesController.php
+++ home/savedmatches/savedMatchesController.php
@@ -55,4 +55,21 @@ $serverName = "4633-project-server.database.windows.net";
     if( $conn === false ) {
         die( print_r( sqlsrv_errors(), true));
     };
+    $username = $_POST["username"];
+    $tsql = "SELECT * FROM [dbo].[Matches]
+        WHERE Username = '$username'";
+    $getResults= sqlsrv_query($conn, $tsql);
+    if ($getResults === false) {
+        echo (sqlsrv_errors());
+    }
+    $schedule = new Schedule();
+    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_BOTH)) {
+        $team1 = $row['Team1'];
+        $team2 = $row['Team2'];
+        $location = $row['Location'];
+        $date = $row['MatchDate'];
+        $match = new Match($team1, $team2, $location, $date);
+        $schedule.addMatch($match);
+    }
+    sqlsrv_free_stmt($getResults);
 ?>
