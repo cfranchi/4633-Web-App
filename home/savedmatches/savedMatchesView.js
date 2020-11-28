5e5574e298c391ab5b3119970c27d58247daf701
cfranchi <cameron.franchi3@gmail.com>
2020-11-28 13:25:03 -0500

    Add files via upload

--- home/savedmatches/savedMatchesView.js
+++ home/savedmatches/savedMatchesView.js
@@ -27,4 +27,30 @@ $(document).ready(function() {
             );
         }
     });
+    $("#saved_matches_div").on('click', '#refresh_saved_matches_button', function () {
+        $.ajax({
+            url: './home/savedmatches/savedMatchesController.php',
+            type: 'post',
+            data: {
+                username: username
+            },
+            success: function(response) {
+                var data = $.parseJSON(response);
+                tableName = "saved_matches_table";
+                displayResults(tableName, 
+                    (tableName) => {
+                        for (var i = 0; i < data.length; i++) {
+                            var tr = "<tr>";
+                            var td0 = "<td></td>";
+                            var td1 = "<td>" + data[i]["team1"] + "</td>";
+                            var td2 = "<td>" + data[i]["team2"] + "</td>";
+                            var td3 = "<td>" + data[i]["location"] + "</td>";
+                            var td4 = "<td>" + data[i]["date"] + "</td></tr>";
+                            $("#" + tableName).append(tr +td0 + td1 + td2 + td3 + td4); 
+                        }
+                    }
+                );
+            }
+        });
+    });
 });
