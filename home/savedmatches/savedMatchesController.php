3717c78fdeff300026e2d89cdb6d6ddf5cd1201f
cfranchi <cameron.franchi3@gmail.com>
2020-11-28 12:33:14 -0500

    Update savedMatchesController.php

--- home/savedmatches/savedMatchesController.php
+++ home/savedmatches/savedMatchesController.php
@@ -28,48 +28,4 @@ class Match {
         return $this->date;
     }
 }
-
-class Schedule {
-    public $matches;
-
-    public function __construct() {
-        $this->matches = [];
-    }
-
-    public function addMatch($match) {
-        $this->matches[] = $match;
-    }
-
-    public function returnMatches() {
-        return $this->matches;
-    }
-}
-
-    $serverName = "4633-project-server.database.windows.net";
-    $connectionOptions = array(
-        "Database" => "4633-Web-App",
-        "Uid" => "clouddev",
-        "PWD" => "password1!"
-    );
-    $conn = sqlsrv_connect($serverName, $connectionOptions);
-    if( $conn === false ) {
-        die( print_r( sqlsrv_errors(), true));
-    };
-    $username = $_POST["username"];
-    $tsql = "SELECT * FROM [dbo].[Matches]
-        WHERE Username = '$username'";
-    $getResults= sqlsrv_query($conn, $tsql);
-    if ($getResults === false) {
-        echo (sqlsrv_errors());
-    }
-    $schedule = new Schedule();
-    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_BOTH)) {
-        $team1 = $row['Team1'];
-        $team2 = $row['Team2'];
-        $location = $row['Location'];
-        $date = $row['MatchDate'];
-        $match = new Match($team1, $team2, $location, $date);
-        $schedule.addMatch($match);
-    }
-    sqlsrv_free_stmt($getResults);
 ?>
