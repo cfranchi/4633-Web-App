b2ffba7538232a5075e6b745b8025fa7238b6242
cfranchi <cameron.franchi3@gmail.com>
2020-11-28 11:00:24 -0500

    Add files via upload

--- home/search/searchbox/searchBoxView.js
+++ home/search/searchbox/searchBoxView.js
@@ -12,7 +12,21 @@ $(document).ready(function() {
                     team1: team1
                 },
                 success: function(response) {
-                    saveResults(response, displayResults(response, "search_results_table", () => {
+                    var data = $.parseJSON(response);
+                    var save_button_arr = [];
+                    saveResults(response, displayResults(response, "search_results_table", 
+                    () => {
+                        for (var i = 0; i < data.length; i++) {
+                            var tr = "<tr>";
+                            var save_button = "<td><button id='add_button_" + i + "'>save</button></td>";
+                            var td1 = "<td>" + data[i]["team1"] + "</td>";
+                            var td2 = "<td>" + data[i]["team2"] + "</td>";
+                            var td3 = "<td>" + data[i]["location"] + "</td>";
+                            var td4 = "<td>" + data[i]["date"] + "</td></tr>";
+                            save_button_arr.push(save_button);
+                            $("#" + tableName).append(tr + save_button + td1 + td2 + td3 + td4); 
+                        }
+                        return save_button_arr;
                     })
                     );
                 }
--- home/search/searchresults/helpers/displayResultsHelper.js
+++ home/search/searchresults/helpers/displayResultsHelper.js
@@ -1,8 +1,6 @@
 function displayResults(response, tableName, callback = () => {}) {
     $("#" + tableName + "_div").html("");
-    var data = $.parseJSON(response);
     var tbl = $("<table/>").attr("id", tableName);
-    var save_button_arr = [];
     $("#" + tableName + "_div").append(tbl);
     var tr = "<tr>";
     var th0 = "<th></th>";
@@ -11,16 +9,5 @@ function displayResults(response, tableName, callback = () => {}) {
     var th3 = "<th>Location</th>";
     var th4 = "<th>Date</th></tr>";
     $("#" + tableName).append(tr + th0 + th1 + th2 + th3 + th4); 
-    for (var i = 0; i < data.length; i++) {
-        var tr = "<tr>";
-        var save_button = "<td><button id='add_button_" + i + "'>save</button></td>";
-        var td1 = "<td>" + data[i]["team1"] + "</td>";
-        var td2 = "<td>" + data[i]["team2"] + "</td>";
-        var td3 = "<td>" + data[i]["location"] + "</td>";
-        var td4 = "<td>" + data[i]["date"] + "</td></tr>";
-        save_button_arr.push(save_button);
-        $("#" + tableName).append(tr + save_button + td1 + td2 + td3 + td4); 
-    }
     callback();
-    return save_button_arr;
 }
