4ab1cb5004c640f808cf9d80d7eacbac100ea1b9
cfranchi <cameron.franchi3@gmail.com>
2020-11-28 01:44:48 -0500

    Add files via upload

--- home/savedmatches/savedMatchesView.js
+++ home/savedmatches/savedMatchesView.js
@@ -1,3 +1,16 @@
 $(function() {
     $("#saved_matches_div").load("./home/savedmatches/savedMatchesView.html"); 
 });
+
+$(document).ready(function() {
+    $.ajax({
+        url: './home/savedmatches/savedMatchesController.php',
+        type: 'post',
+        data: {
+            username: username
+        },
+        success: function(response) {
+
+        }
+    });
+});
