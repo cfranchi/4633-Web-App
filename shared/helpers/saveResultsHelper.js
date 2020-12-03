1e00a152781e945d2b48c0e4b3a4e4eb8b93993b
cfranchi <cameron.franchi3@gmail.com>
2020-12-02 21:32:22 -0500

    Update saveResultsHelper.js

--- shared/helpers/saveResultsHelper.js
+++ shared/helpers/saveResultsHelper.js
@@ -2,6 +2,8 @@ function saveResults(response, save_button_arr) {
     var data = $.parseJSON(response);
     for (var i = 0; i < save_button_arr.length; i++) {
         $("#add_button_" + i).click(function() {
+            i = i - 1;
+            console.log("i = " + i);
             var team1 = data[i]["team1"];
             var team2 = data[i]["team2"];
             var location = data[i]["location"];
