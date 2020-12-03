a4e6e7d599c105504afa176bf72fb96bd6c09da7
cfranchi <cameron.franchi3@gmail.com>
2020-12-02 22:57:32 -0500

    Add files via upload

--- shared/helpers/saveResultsHelper.js
+++ shared/helpers/saveResultsHelper.js
@@ -1,9 +1,36 @@
 function saveResults(response, save_button_arr) {
     var data = $.parseJSON(response);
     for (var i = 0; i < save_button_arr.length; i++) {
-        var button = document.getElementById("#add_button_" + i);
-        console.log(button);
-        $("#add_button_" + i).click(function() {
+        console.log("closure");
+        $("#add_button_" + i).click = function (num) {
+            return function () {
+                var team1 = data[num]["team1"];
+                var team2 = data[num]["team2"];
+                var location = data[num]["location"];
+                var date = data[num]["date"];
+                $.ajax({
+                    url: './home/search/searchresults/searchResultsController.php',
+                    type: 'post',
+                    data: {
+                        username: username,
+                        team1: team1,
+                        team2: team2,
+                        location: location,
+                        date: date
+                    },
+                    success: function(response) {
+                        if (response == 0) {
+                            alert("match saved");
+                        } else {
+                            alert("error occured!");
+                        }
+                    }
+                });
+            };
+        }(i);
+
+
+        /*$("#add_button_" + i).click(function() {
             console.log("i am button " + i);
             i = i - 1;
             var team1 = data[i]["team1"];
@@ -28,6 +55,6 @@ function saveResults(response, save_button_arr) {
                     }
                 }
             });
-        });
+        });*/
     }
 }
